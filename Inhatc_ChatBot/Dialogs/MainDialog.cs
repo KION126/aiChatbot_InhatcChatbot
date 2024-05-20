e75698e36c7c3fd76cb8de21a48867631876843e
hwangjihong <wlghd6737@gmail.com>
2024-05-20 21:40:32 +0900

    초기 커밋.

---
+++ Inhatc_ChatBot/Dialogs/MainDialog.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Inhatc_ChatBot.Clu;
+using Inhatc_ChatBot.CognitiveModels;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace Inhatc_ChatBot.Dialogs
+{
+    public class MainDialog : ComponentDialog
+    {
+        private readonly InhatcRecognizer _cluRecognizer;
+        protected readonly ILogger Logger;
+
+        // Dependency injection uses this constructor to instantiate MainDialog
+        public MainDialog(InhatcRecognizer cluRecognizer, ILogger<MainDialog> logger)
+            : base(nameof(MainDialog))
+        {
+            _cluRecognizer = cluRecognizer;
+            Logger = logger;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                IntroStepAsync,
+                ActStepAsync,
+
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> IntroStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            if (!_cluRecognizer.IsConfigured)
+            {
+                await stepContext.Context.SendActivityAsync(
+                    MessageFactory.Text("NOTE: CLU is not configured. To enable all capabilities, add 'CluProjectName', 'CluDeploymentName', 'CluAPIKey' and 'CluAPIHostName' to the appsettings.json file.", inputHint: InputHints.IgnoringInput), cancellationToken);
+
+                return await stepContext.NextAsync(null, cancellationToken);
+            }
+
+            // Use the text provided in FinalStepAsync or the default if it is the first time.
+            return await stepContext.NextAsync(null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ActStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+
+
+            // Call CLU and gather any potential booking details. (Note the TurnContext has the response to the prompt.)
+            var cluResult = await _cluRecognizer.RecognizeAsync<Inhatc>(stepContext.Context, cancellationToken);
+            switch (cluResult.GetTopIntent().intent)
+            {
+                case Inhatc.Intent.Hi:
+                    var HiMessageText = $"안녕하세요 테스트 성공입니다.";
+                    var HiMessage = MessageFactory.Text(HiMessageText,
+                        HiMessageText, InputHints.IgnoringInput);
+                    await stepContext.Context.SendActivityAsync(HiMessage, cancellationToken);
+
+                    break;
+
+                case Inhatc.Intent.Food:
+                    var HiMessageText1 = $"안녕하세요 테asdasdad스트 성공입니다.";
+                    var HiMessage1 = MessageFactory.Text(HiMessageText1,
+                        HiMessageText1, InputHints.IgnoringInput);
+                    await stepContext.Context.SendActivityAsync(HiMessage1, cancellationToken);
+                    break;
+
+                default:
+                    // Catch all for unhandled intents
+                    var didntUnderstandMessageText = $"Sorry, I didn't get that. Please try asking in a different way (intent was {cluResult.GetTopIntent().intent})";
+                    var didntUnderstandMessage = MessageFactory.Text(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
+                    await stepContext.Context.SendActivityAsync(didntUnderstandMessage, cancellationToken);
+                    break;
+            }
+
+            return await stepContext.NextAsync(null, cancellationToken);
+        }
+    }
+}
